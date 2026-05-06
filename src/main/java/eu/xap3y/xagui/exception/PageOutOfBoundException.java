a9655210c40e877ff5ed61dfe0838ee8849e1f8d
Martin Hoke <58666857+xap3y@users.noreply.github.com>
2026-05-06 16:16:47 +0200

    1.5.0

--- src/main/java/eu/xap3y/xagui/exception/PageOutOfBoundException.java
+++ src/main/java/eu/xap3y/xagui/exception/PageOutOfBoundException.java
@@ -1,4 +1,4 @@
-package eu.xap3y.xagui.internal.exception;
+package eu.xap3y.xagui.exception;
 
 public class PageOutOfBoundException extends RuntimeException {
     public PageOutOfBoundException(String message) {
