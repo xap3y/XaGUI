94c6f1447e2987f72f1510f96720693912653d23
panneiphyu <58666857+xap3y@users.noreply.github.com>
2026-08-19 19:54:24 +0200

    new docs

--- paper/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"eu.xap3y.xagui.paper","l":"PaperAdapter"},{"p":"eu.xap3y.xagui.paper","l":"ParseUtil"},{"p":"eu.xap3y.xagui.paper","l":"XaGuiCommandPaper"}];updateSearchResults();
